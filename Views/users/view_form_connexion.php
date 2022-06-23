f96810dedfc352e982f938d72ad0d38f1a5508c7
AAmboise <adrien.amboise@yahoo.fr>
2022-06-23 10:47:56 +0200

    creation base.php

--- Views/users/view_form_connexion.php
+++ Views/users/view_form_connexion.php
@@ -1,7 +1,19 @@
+<?php
+$title ="back-office Connexion";
+$css = '<link rel="stylesheet" href="styleConnexion.css">';
+ob_start();
+?>
+
+
 <h2>connexion</h2>
 <form method="post">
-    <input type="text" name="email" id="email">
-    <input type="password" name="password" id="password">
+    <input type="text" name="email" id="email" placeholder="Votre Email">
+    <input type="password" name="password" id="password" placeholder="Votre mot de passe">
     <button>se connecter</button>
 </form>
 <a href="register">Creer un compte</a>
+
+<?php
+$content = ob_get_clean();
+require_once './views/base.php';
+?>
